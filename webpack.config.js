c991db9550ffa1c3f9175370098e7280f1c993e2
matteoAmico <mamico@globaleagleent.com>
2017-04-10 14:33:47 +0100

    added webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,13 @@
+var webpack = require('webpack');
+
 module.exports = {
-    
+    context: __dirname,
+    entry: {
+        'public/js/app' : __dirname + "/assets/js/app.js",
+        'modules/application/public/js/application' : __dirname + "/modules/application/resources/assets/js/module.js"
+    },
+    output: {
+        path: __dirname + '/',
+        filename: "[name].min.js"
+    }
 }
