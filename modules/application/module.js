2844eb411d85ecc1ff6a89aded5053aba79af0d7
matteoAmico <mamico@globaleagleent.com>
2017-04-10 11:59:42 +0100

    added babel

--- modules/application/module.js
+++ modules/application/module.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require('path');
 var info = require('./application.json');
 var config = require('./config/config')
-var indexRoute = require('./routes/index');
+var webRoutes = require('./routes/web');
 
 var app = express();
 
 //routes
-app.use(indexRoute);
+app.use(webRoutes);
 //give access to module public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
