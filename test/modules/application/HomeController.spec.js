2bc8162454e46242ca1ed4f8b58127019f0caee6
matteoAmico <mamico@globaleagleent.com>
2017-04-10 17:08:32 +0100

    added test

---
+++ test/modules/application/HomeController.spec.js
@@ -0,0 +1,10 @@
+var chai = require('chai')
+var expect = require('chai').expect
+
+var HomeController = require('../../../modules/application/controllers/HomeController')
+
+describe('HomeController',function(){
+    it('assert true is true',function(){
+        expect(true).to.equal(true)
+    })
+})
